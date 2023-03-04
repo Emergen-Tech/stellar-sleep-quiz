e754bc9651df74aba7ef19d96898ef94f3a426df
Huzaifa Imran <huzaifadsu@gmail.com>
2023-03-05 03:12:23 +0500

    revisions 1 and 5

--- components/quizTypes/MultipleChoiceQuiz.js
+++ components/quizTypes/MultipleChoiceQuiz.js
@@ -1,8 +1,7 @@
-import { useState } from "react";
 // import ProgressBar from "./ProgressBar";
 // import { AiOutlineArrowLeft } from "react-icons/ai";
-import { moveToNextQuestion, setAnswer } from "@/reducers/QuizSlice";
-import { useDispatch, useSelector } from "react-redux";
+import { moveToNextQuestion, setAnswer } from '@/reducers/QuizSlice';
+import { useDispatch, useSelector } from 'react-redux';
 
 export default function MultipleChoiceQuiz() {
   const current = useSelector(
@@ -15,6 +14,7 @@ export default function MultipleChoiceQuiz() {
 
   function handleOptionClick(option) {
     dispatch(setAnswer([option]));
+    dispatch(moveToNextQuestion());
   }
 
   function handleSubmitResponse() {
@@ -30,42 +30,40 @@ export default function MultipleChoiceQuiz() {
 
   const props = {
     step: { page_name },
-    flowId: "savvy",
+    flowId: 'savvy',
   };
   if (
-    typeof window !== "undefined" &&
+    typeof window !== 'undefined' &&
     window.rudderanalytics &&
-    typeof window.rudderanalytics.track === "function"
+    typeof window.rudderanalytics.track === 'function'
   ) {
     window.rudderanalytics.track(`start sleep quiz`, props);
   }
 
   return (
     <>
-      <div className="w-[350px] mx-auto">
-        <h1 className="text-[25px] text-[#ffffff] py-4">{question}</h1>
-        <div className="grid gap-2">
+      <div className='w-[350px] mx-auto'>
+        <h1 className='text-[25px] text-[#ffffff] py-4'>{question}</h1>
+        <div className='grid gap-2'>
           {Object.entries(choices).map((entry) => (
-            <div key={entry[0]} className="mb-2">
+            <div key={entry[0]} className='mb-2'>
               <button
                 onClick={() => handleOptionClick(entry[0])}
                 className={`w-full text-center hover:scale-[1.02] transition-all h-[65px] rounded-[10px] px-7 text-[17px] text-[#ffffff]
                 ${
                   entry[0] === currentAnswerVar
-                    ? "bg-[#769E7D]"
-                    : "bg-[#37533C]"
-                }`}
-              >
+                    ? 'bg-[#769E7D]'
+                    : 'bg-[#37533C]'
+                }`}>
                 {entry[1]}
               </button>
             </div>
           ))}
         </div>
-        <div className="w-[350px]">
+        <div className='w-[350px]'>
           <button
             onClick={() => handleSubmitResponse()}
-            className="bg-[#DE8F6E] w-full h-[70px] text-white text-[20px] text-center rounded-[10px] my-[20px]"
-          >
+            className='bg-[#DE8F6E] w-full h-[70px] text-white text-[20px] text-center rounded-[10px] my-[20px]'>
             Next
           </button>
         </div>
